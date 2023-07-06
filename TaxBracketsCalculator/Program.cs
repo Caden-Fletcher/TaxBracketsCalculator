d053a204b303df02c524552cd3cde1a4cb568d01
Caden-Fletcher <110023835+Caden-Fletcher@users.noreply.github.com>
2023-07-06 14:06:44 -0500

    Add project files.

---
+++ TaxBracketsCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TaxBracketsCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalIncome = 0;
+
+            Console.WriteLine("Please enter the total income you made in 2023: ");
+            totalIncome = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(totalIncome);
+        }
+    }
+}
